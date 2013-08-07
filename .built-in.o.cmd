30db9a30243f244ea85a98211b9f655dcd144a20
bobuhiro11 <blackcherry.mn@gmail.com>
2013-08-07 10:49:24 -0400

    exec acccess control added.

---
+++ .built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_security/bobulsm/built-in.o :=  ld -m elf_i386   -r -o security/bobulsm/built-in.o security/bobulsm/bobulsm.o 
--- bobulsm.c
+++ bobulsm.c
@@ -7,6 +7,8 @@
 #include <linux/security.h>
 #include <linux/xattr.h>
 #include <linux/in.h>
+#include <linux/cred.h>
+#include <linux/binfmts.h>
 #include <linux/capability.h>
 #include <linux/unistd.h>
 #include <linux/mm.h>
@@ -53,8 +55,17 @@
 #include <linux/mutex.h>
 #include <linux/posix-timers.h>
 
-static int __init bobulsm_init(void);
+#define BUFLEN	512		/* buffer size for d_absolute_path */
+
 static int bobulsm_inode_mkdir(struct inode *inode, struct dentry *dentry,umode_t mask);
+static int bobulsm_bprm_set_creds(struct linux_binprm *bprm);
+static int bobulsm_bprm_check_security(struct linux_binprm *bprm);
+static int bobulsm_cred_alloc_blank(struct cred *new, gfp_t gfp);
+static int bobulsm_cred_prepare(struct cred *new, const struct cred *old, gfp_t gfp);
+static void bobulsm_cred_transfer(struct cred *new, const struct cred *old);
+static void bobulsm_cred_free(struct cred *cred);
+
+static int __init bobulsm_init(void);
 static int bobulsm_securityfs_init(void);
 static void bobulsm_securityfs_exit(void);
 static ssize_t write_policy(struct file *filep,const char __user *buf,size_t count,loff_t *ppos);
@@ -77,9 +88,31 @@ static struct file_operations bobulsm_file_ops = {
  * operations of lsm hooks
  */ 
 static struct security_operations bobulsm_ops = {
-	.name = "bobulsm",
-	.inode_mkdir = bobulsm_inode_mkdir,
+	.name                = "bobulsm",
+	.inode_mkdir         = bobulsm_inode_mkdir,
+	.bprm_set_creds      = bobulsm_bprm_set_creds,
+	.bprm_check_security = bobulsm_bprm_check_security,
+	.cred_alloc_blank    = bobulsm_cred_alloc_blank,
+	.cred_prepare        = bobulsm_cred_prepare,
+	.cred_transfer       = bobulsm_cred_transfer,
+	.cred_free           = bobulsm_cred_free,
 };
+
+/*
+ * domain information used via bprm->cred->security
+ */
+struct domain {
+	bool flag;
+};
+
+static struct domain domain1 = {
+	.flag = false,
+};
+
+static struct domain domain2 = {
+	.flag = true,
+};
+
 
 
 
@@ -89,15 +122,18 @@ static struct security_operations bobulsm_ops = {
  * make securityfs and policy file
  * /sys/kernel/security/bobulsm/policy
  */
-static int bobulsm_securityfs_init(void){
+static int bobulsm_securityfs_init(void)
+{
 	int rc = 0;
 	
-	// make "bobulsm" directory
+	/* make "bobulsm" directory */
 	bobulsm_dentry = securityfs_create_dir("bobulsm",NULL);
 	if( IS_ERR(bobulsm_dentry))
 		return PTR_ERR(bobulsm_dentry);
-	// make "policy" file	
-	// read and write enabled.
+	/* 
+	 * make "policy" file	
+	 * read and write enabled. 
+	 */
 	policy_dentry = securityfs_create_file(
 		"policy",S_IRUGO|S_IWUSR,bobulsm_dentry,NULL,&bobulsm_file_ops);
 	if( IS_ERR(policy_dentry)){
@@ -111,7 +147,8 @@ static int bobulsm_securityfs_init(void){
  * remove securityfs and policy file
  * /sys/kernel/security/bobulsm/policy
  */
-static void bobulsm_securityfs_exit(void){
+static void bobulsm_securityfs_exit(void)
+{
 	securityfs_remove(policy_dentry);
 	securityfs_remove(bobulsm_dentry);
 }
@@ -121,7 +158,8 @@ static void bobulsm_securityfs_exit(void){
  * read policy
  */ 
 static ssize_t read_policy(struct file *filep,char __user *buf,
-	size_t count,loff_t *ppos){
+	size_t count,loff_t *ppos)
+{
 	
 	char tmp[256];
 	ssize_t len;
@@ -130,7 +168,7 @@ static ssize_t read_policy(struct file *filep,char __user *buf,
 		"This is bobulsm policy file.\nAmazinggggggg :D :D :D\n");	
 	
 	printk("bobulsm: policy read.\n");
-	// copy from "tmp(kernel buffer)" to "buf(user buffer)"
+	/* copy from "tmp(kernel buffer)" to "buf(user buffer)" */
 	return simple_read_from_buffer(buf,count,ppos,tmp,len);
 }
 
@@ -138,7 +176,8 @@ static ssize_t read_policy(struct file *filep,char __user *buf,
  * write policy
  */ 
 static ssize_t write_policy(struct file *filep,const char __user *buf,
-	size_t count,loff_t *ppos){
+	size_t count,loff_t *ppos) 
+{
 
 	char tmp[256];
 	ssize_t len = sizeof(tmp);
@@ -158,38 +197,139 @@ static ssize_t write_policy(struct file *filep,const char __user *buf,
 }
 	
 /*
- * lsm hook
+ * lsm hook for inode_mkdir
  */ 
 static int bobulsm_inode_mkdir(struct inode *inode, struct dentry *dentry,
-			   umode_t mask){
-	printk("bobulsm: inode_mkdir called.\n");
+			   umode_t mask)
+{
+	//printk("bobulsm: inode_mkdir called.\n");
+
+  	/* Return 0 if permission is granted. */
+	return 0;
+}
+
+/*
+ * lsm hook for cred_alloc_blank
+ */
+static int bobulsm_cred_alloc_blank(struct cred *new, gfp_t gfp)
+{
+	new->security = NULL;
+	return 0;
+}
+
+/*
+ * lsm hook for cred_prepare
+ */
+static int bobulsm_cred_prepare(struct cred *new, const struct cred *old, gfp_t gfp)
+{
+	new->security = old->security;
+	return 0;
+}
+
+/*
+ * lsm hook for cred_transfer
+ */
+static void bobulsm_cred_transfer(struct cred *new, const struct cred *old)
+{
+	bobulsm_cred_prepare(new,old,0);
+}
+
+/*
+ * lsm hook for cred_free
+ */
+static void bobulsm_cred_free(struct cred *cred)
+{
+	return;	
+}
+
+/*
+ * lsm hook for bprm_set_creds
+ */
+static int bobulsm_bprm_set_creds(struct linux_binprm *bprm)
+{
+	const struct cred *old = (struct cred *)current_cred();
+	struct cred *new = bprm->cred;
+	int rc;
+	
+	rc = cap_bprm_set_creds(bprm);
+	if (rc)
+		return rc;
+
+	if (bprm->cred_prepared)
+		return 0;
+	/*
+	if(strstr(bprm->filename,"bobu")){
+		printk("bobulsm_bprm_set_creds: bprm->filename \"%s\"\n",
+			bprm->filename);
+		if(bprm->cred->security)
+			printk("bobulsm_bprm_set_creds: bprm->cred->security->flag \"%d\"\n",
+				((struct domain*)bprm->cred->security)->flag);
+		else
+			printk("bobulsm_bprm_set_creds: bprm->cred->security is NULL\n");
+	}
+	*/
+
+  	/* Return 0 if permission is granted. */
+	return rc;
+}
+
+/*
+ * lsm hook for bprm_check_security
+ */
+static int bobulsm_bprm_check_security(struct linux_binprm *bprm)
+{
+	char buf[BUFLEN];
+	char *pos = ERR_PTR(-ENOMEM);	
+	struct cred *new = bprm->cred;
+
+	pos = d_absolute_path(&bprm->file->f_path,buf,BUFLEN);
+	if(IS_ERR(pos))
+		return pos;
+
+	if(strstr(bprm->filename,"bobu")){
+		new->security = &domain2;	
+		printk("bobulsm_bprm_check_security: bprm->filename \"%s\"\n",
+			bprm->filename);
+		if(bprm->cred->security)
+			printk("bobulsm_bprm_check_security: bprm->cred->security->flag \"%d\"\n",
+				((struct domain*)bprm->cred->security)->flag);
+		else
+			printk("bobulsm_bprm_check_security: bprm->cred->security is NULL\n");
+	}
+	else if(strstr(bprm->filename,"hoge")){
+		printk("bobulsm_bprm_check_security: bprm->filename \"%s\"\n",
+			bprm->filename);
+		printk("bobulsm_bprm_check_security: absolute_path \"%s\"\n",
+			buf);
+		if(bprm->cred->security)
+			printk("bobulsm_bprm_check_security: bprm->cred->security->flag \"%d\"\n",
+				((struct domain*)bprm->cred->security)->flag);
+		else
+			printk("bobulsm_bprm_check_security: bprm->cred->security is NULL\n");
+	}
 
   	/* Return 0 if permission is granted. */
 	return 0;
 }
 
 
-
-static int __init bobulsm_init(void){
+static int __init bobulsm_init(void)
+{
 	int rc = 0;
+	struct cred *cred = (struct cred *) current_cred();
 
 	if (! security_module_enable(&bobulsm_ops)){
 		printk("bobulsm: did not selected.\n");
 		return 0;
 	}
 
-	if (register_security(&bobulsm_ops)){
+	if (register_security(&bobulsm_ops))
 		panic("bobulsm: Occurred an error at register_security(&bobulsm_ops).\n");
-	}else{
+	else
 		printk("bobulsm: registered surely.\n");
-	}
 
-	/*
-	if( rc = bobulsm_securityfs_init() ){
-		printk("bobulsm: Occurred an error at bobulsm_securityfs_init()"); 
-		return rc;
-	}
-	*/
+	cred->security = &domain1;
+
 	return 0;
 }
 
